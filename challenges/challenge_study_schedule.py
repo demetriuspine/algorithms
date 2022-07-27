c26d2db5f7b8936e99bf1bc9644547548497a2d3
lucas <mr.pine97@gmail.com>
2022-07-27 16:46:10 -0300

    req01 - feat: implementa a função study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    counter = 0
+
+    try:
+
+        for start_period, end_period in permanence_period:
+            if start_period <= target_time <= end_period:
+                counter += 1
+
+        return counter
+
+    except TypeError:
+        return None
