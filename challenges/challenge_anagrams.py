8aa26330745ed00afefc0ab9a77b4e6025f13c35
lucas <mr.pine97@gmail.com>
2022-07-27 17:28:59 -0300

    req03 - linter: corrige issues diversos

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -6,14 +6,16 @@ def is_anagram(first_string, second_string):
     lowered_first_string = first_string.lower()
     lowered_second_string = second_string.lower()
 
-    first_string_list = list(lowered_first_string) ## https://www.delftstack.com/howto/python/python-string-to-list/
-    # print(first_string_list)
-    second_string_list = list(lowered_second_string) ## https://www.delftstack.com/howto/python/python-string-to-list/
-    # print(second_string_list)
+    # https://www.delftstack.com/howto/python/python-string-to-list/
+    first_string_list = list(lowered_first_string)
+
+    # https://www.delftstack.com/howto/python/python-string-to-list/
+    second_string_list = list(lowered_second_string)
 
     for letter in first_string_list:
         if letter in second_string_list:
-            second_string_list.remove(letter) # https://delftstack.com/howto/python/delete-element-from-list-by-value/
+            # https://delftstack.com/howto/python/delete-element-from-list-by-value/
+            second_string_list.remove(letter)
         else:
             return False
 
