17ed9042b339cd2bcf135aff2c206e95844b55be
lucas <mr.pine97@gmail.com>
2022-07-27 17:28:35 -0300

    req02 - linter: corrige 'blank line contains whitespace'

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,7 +2,7 @@ def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
     if word == '':
         return False
-    
+
     if len(word) == 1:
         return True
 
